f3779364f3006664001343cacd2a9e1b6f4a58f2
Rocio Masjuan <rmasjuan@itba.edu.ar>
2026-03-05 16:57:41 -0300

    ejemplo

--- main.py
+++ main.py
@@ -15,3 +15,21 @@ if __name__ == "__main__":
 #cambio
 
 print("cambio")
+
+
+import random
+
+#Juego Generala 
+
+#5 dados + 2 jugadores
+
+#TURNOS 
+#3 turnos, cada turno 5 dados 
+def tirada_dados(cantidad):
+    for dado in range(1, cantidad):
+        dados = []
+        dados =+ random.randint(1,6)
+    return dados
+
+ejemplo = tirada_dados(5)
+print(ejemplo)
