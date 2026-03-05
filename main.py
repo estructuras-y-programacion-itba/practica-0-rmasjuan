cd63d20fbdf1abb465261cc51b66148c1345d699
Rocio Masjuan <rmasjuan@itba.edu.ar>
2026-03-05 17:56:24 -0300

    cree la funcion jugar turno

--- main.py
+++ main.py
@@ -26,10 +26,44 @@ import random
 #TURNOS 
 #3 turnos, cada turno 5 dados 
 def tirada_dados(cantidad):
-    for dado in range(1, cantidad):
-        dados = []
-        dados =+ random.randint(1,6)
+    dados = []
+    for dado in range(0, cantidad):
+        dados.append(random.randint(1,6)) 
     return dados
 
-ejemplo = tirada_dados(5)
-print(ejemplo)
+tirada = tirada_dados(5)
+
+def jugar_turno(jugador, planilla):
+    cantidad = 0
+    dados = 5
+    tirada = tirada_dados(dados)
+    while cantidad < 3:
+        print(tirada)
+        continuar = input("Queres cambiar los dados (si/no)")
+        if continuar == "no":
+            cantidad = 3
+        else:
+            cantidad += 1
+            cambios = input("Que posiciones queres cambiar (0, 1, 2, 3, 4)")
+            indices = [int(i) for i in cambios.split() if i.isdigit()]
+            for i in indices:
+                if 0 <= i < len(tirada):
+                    tirada[i] = random.randint(1,6)
+                    
+        tirada.sort()
+        print("tu tirada final es:", tirada)
+    return tirada
+
+jug = 1
+opciones = ["1", "2", "3", "4", "5", "6", "E", "F", "P", "G"]
+plantilla =[]
+
+jugar_turno(jug, plant)
+
+def llenar_plantilla(tirada;plantilla):
+    tirada.sort()
+    cantidades = [0,0,0,0,0]
+    for i in tirada:
+        cantidades[i-1] += 1
+    for i in cantidades:
+        
